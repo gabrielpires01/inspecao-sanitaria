537c2e05175ec2ff2ad20571c294eb7dd03b2432
Gabriel Pires <gabrielpires@poli.ufrj.br>
2026-02-21 19:04:38 -0300

    feat: Add role to users

---
+++ backend/app/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class RoleEnum(Enum):
+    superuser = 1, "superuser"
+    inspector = 2, "inspector"
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from app.enums import RoleEnum
+from sqlalchemy import Column, Integer, String, DateTime, Enum
 from sqlalchemy.sql import func
 from app.core.database import Base
 
@@ -7,9 +8,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     full_name = Column(String, nullable=True)
+    role = Column(Enum(RoleEnum), default=RoleEnum.inspector)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
